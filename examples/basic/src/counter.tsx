3ba0818332903c4ba5d568e9578a26ab4a80890d
Duke <duc@silentium.io>
2024-11-29 17:26:33 +0700

    fix: version pump

--- examples/basic/src/counter.tsx
+++ examples/basic/src/counter.tsx
@@ -1,4 +1,4 @@
-import { createStreamable } from "streambly-react";
+import { createStream } from "streambly-react";
 import { counter } from "./counter.app";
 
-export const counterStream = createStreamable(counter);
+export const counterStream = createStream(counter);
